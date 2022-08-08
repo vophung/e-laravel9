8e9703511445c06690d75f220690f896a9f94edf
vophung <vophung20112000@gmail.com>
2022-08-08 23:27:20 +0700

    Revamp auth heading

--- resources/views/auth/blocks/head.blade.php
+++ resources/views/auth/blocks/head.blade.php
@@ -6,4 +6,21 @@
 <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
 <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
 <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+<link rel="stylesheet" href="{{ asset('assets/css/pages/fontawesome.css') }}">
+<link rel="stylesheet" href="{{asset('assets/css/widgets/toastr.min.css')}}">
+<style>
+    .fontawesome-icons {
+        text-align: center;
+    }
+
+    article dl {
+        background-color: rgba(0, 0, 0, .02);
+        padding: 20px;
+    }
+
+    .fontawesome-icons .the-icon {
+        font-size: 24px;
+        line-height: 1.2;
+    }
+</style>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
