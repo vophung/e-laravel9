ff05a3ef0b19697499dff1fb1577e532920e899b
vophung <vophung20112000@gmail.com>
2022-08-08 23:42:14 +0700

    error page

--- resources/views/403.blade.php
+++ resources/views/403.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 - Mazer Admin Dashboard</title>
+    <title>403 - Mazer Admin Dashboard</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/pages/error.css') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
@@ -18,9 +18,9 @@
 <div class="error-page container">
     <div class="col-md-8 col-12 offset-md-2">
         <div class="text-center">
-            <img class="img-error" src="{{ asset('assets/images/samples/error-404.svg') }}" alt="Not Found">
-            <h1 class="error-title">NOT FOUND</h1>
-            <p class='fs-5 text-gray-600'>The page you are looking not found.</p>
+            <img class="img-error" src="{{ asset('assets/images/samples/error-403.svg') }}" alt="Not Found">
+            <h1 class="error-title">Forbidden</h1>
+            <p class="fs-5 text-gray-600">You are unauthorized to see this page.</p>
             <a href="index.html" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
         </div>
     </div>
