db63c0d00245569b883bec434f766e9c5897e541
vophung <vophung20112000@gmail.com>
2022-08-08 23:41:41 +0700

    Mail view

--- resources/views/mail/successfull.blade.php
+++ resources/views/mail/successfull.blade.php
@@ -1,1 +1,16 @@
-<h1>Sent Mails</h1>
+@extends('auth.master')
+@section('content')
+<div id="auth-left">
+    <div class="auth-logo">
+        <a href="index.html"><img src="{{ asset('assets/images/logo/logo.svg') }}" alt="Logo"></a>
+    </div>
+    <h1 class="auth-title">Thank You!</h1>
+    <p class="auth-subtitle mb-5">Welcome {{ $data['email'] }} to our website.</p>
+    <p class="auth-subtitle mb-5">Please check your email to verify your account.</p>
+    <div class="text-center mt-5 text-lg fs-4">
+        <p class="text-gray-600">Do your already have an account? <a href="{{ route('login.index') }}" class="font-bold">Sign
+                in</a>.</p>
+        <p><a class="font-bold" href="#">Forgot password?</a>.</p>
+    </div>
+</div>
+@endsection
